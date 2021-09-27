e69fbe5e892aaa0c477c97b9df7be80abcc71317
Rahul <rj.rahul1908@gmail.com>
2021-09-27 09:53:32 +0530

    Fourth Commit - calc

--- app/src/main/java/com/haxon/larp/fragments/HomeFragment.java
+++ app/src/main/java/com/haxon/larp/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.haxon.larp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,15 +10,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.haxon.larp.Activities.CreateNewTaskActivity;
 import com.haxon.larp.R;
 
 public class HomeFragment extends Fragment {
+
+    FloatingActionButton addTask;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        addTask = view.findViewById(R.id.home_addTask);
+
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(requireContext(), CreateNewTaskActivity.class));
+            }
+        });
+
 
         return view;
 
