8902ac38455972da55ed7d22e598bc288bf5e70b
Rahul <rj.rahul1908@gmail.com>
2021-09-27 08:12:59 +0530

    Third Commit - build nav bar

--- app/src/main/java/com/haxon/larp/Activities/DashboardActivity.java
+++ app/src/main/java/com/haxon/larp/Activities/DashboardActivity.java
@@ -1,16 +1,88 @@
 package com.haxon.larp.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.haxon.larp.R;
+import com.haxon.larp.fragments.HomeFragment;
+import com.haxon.larp.fragments.RulesFragment;
+import com.haxon.larp.fragments.TaskFragment;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home_menu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new HomeFragment()).commit();
+                        break;
+                    case R.id.intruction_menu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new RulesFragment()).commit();
+                        break;
+                    case R.id.task_menu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new TaskFragment()).commit();
+                        break;
+                    case R.id.character_menu:
+                        Toast.makeText(DashboardActivity.this, "Avatar", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_open,R.string.navigation_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.home_menu);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/haxon/larp/fragments/RulesFragment.java
@@ -0,0 +1,26 @@
+package com.haxon.larp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.haxon.larp.R;
+
+public class RulesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_rules,container,false);
+
+        return view;
+
+    }
+
+}
